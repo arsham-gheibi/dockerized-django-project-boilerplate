9251723b85a5c845808f7f41a10f4ebf5eef9ac2
SeelpAydin <aydinjabary98@gmail.com>
2021-12-07 15:16:49 +0330

    update to django 4

--- app/app/settings.py
+++ app/app/settings.py
@@ -57,14 +57,12 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_URL = '/static/'
-MEDIA_URL = "/media/"
+STATIC_URL = 'static/'
+MEDIA_URL = "media/"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
