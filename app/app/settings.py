48613b68d2bba721f9146f15967d8df7733cd6ea
SeelpAydin <aydinjabary98@gmail.com>
2021-12-24 22:15:56 +0330

    add django-debug-toolbar

--- app/app/settings.py
+++ app/app/settings.py
@@ -89,6 +89,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if DEBUG:
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + \
+        ['127.0.0.1', '10.0.2.2'] + ALLOWED_HOSTS
+
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
 
     STATIC_ROOT = "staticfiles"
     MEDIA_ROOT = "mediafiles"
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+def handler_404(request, exception):
+    return redirect('store:home')
+
+
+handler404 = 'app.urls.handler_404'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,3 +21,4 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
